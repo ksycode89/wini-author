9335d4ff290f0c3e5a814cf9a6df16b59f013506
wini <wini@DESKTOP-4EACK70>
2023-03-14 19:51:44 +0900

    03_14_saV_V

---
+++ src/main/java/egovframework/wini/service/impl/eqmnSTAT/EqmnSTATmapper.java
@@ -0,0 +1,16 @@
+package egovframework.wini.service.impl.eqmnSTAT;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EqmnSTATmapper {
+	// 장비 리스트 조회
+	List<Map<String,Object>> getEqmnList();
+		
+	// 통계 데이터 조회
+	List<Map<String, Object>> getStat(Map<String, Object> param);
+	// 통계 데이터 갯수 조회
+	int getStatCnt(Map<String, Object> param);
+	// 통계 'Y' 갯수 조회
+	List<Map<String, Object>> getStatChckResult(Map<String, Object> param);
+}
